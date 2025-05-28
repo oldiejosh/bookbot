7f81273ae00c62773348ea9fd77d4ab7ba832c12
oldiejosh <josholdfield34@gmail.com>
2025-05-27 18:11:14 -0700

    C: character_report

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
+from stats import count_words, count_characters, generate_character_report
+
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         book_text = f.read()
     return book_text
 
-def count_words(book_text):
-    words = book_text.split()
-    return len(words)
 
 def main():
     path_to_file = "/home/josholdfield/bookbot/books/frankenstein.txt"
     book_text = get_book_text(path_to_file)
     word_count = count_words(book_text)
-    print(book_text)
-    print(f"{word_count} words found in the document")
+    character_counts = count_characters(book_text)
+    character_report = generate_character_report(character_counts)
 
+    print(book_text)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for entry in character_report:
+        print(f"{entry['char']}: {entry['num']}")
+    print("============= END ===============")
 
 main()
 
